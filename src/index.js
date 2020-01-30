61c4619283fb8bf5af6fa8fb880adafc441483a8
James Harris <wopian@wopian.me>
2020-01-30 21:50:11 +0000

    chore: add limit option

--- src/index.js
+++ src/index.js
@@ -19,12 +19,10 @@ router.get('/', ctx => {
 
 // http://localhost:3001/inspireid/14835677
 router.get('/inspireid/:id', bodyParser, async ctx => {
-    const sql = db.prepare('SELECT * FROM landRegistry WHERE inspireID=?')
+    const sql = db.prepare('SELECT inspireID, coordinates FROM landRegistry WHERE inspireID=?')
     const data = await sql.get(ctx.params.id)
     if (data) {
         data.coordinates = JSON.parse(data.coordinates)
-        delete data.latitude
-        delete data.longitude
         ctx.body = { data }
     } else {
         ctx.body = {
@@ -36,22 +34,22 @@ router.get('/inspireid/:id', bodyParser, async ctx => {
 
 // http://localhost:3001/nearby/51.6245033595047/-3.93390545244112
 // http://localhost:3001/nearby/51.6245033595047/-3.93390545244112/2
-router.get('/nearby/:latitude/:longitude/:radius?', async ctx => {
+router.get('/nearby/:latitude/:longitude/:limit?', async ctx => {
     // miles = 3959; km = 6371
     //const sql = db.prepare('SELECT inspireID, ( 3959 & acos(cos(radians(@latitude)) * cos(radians(latitude)) * cos(radians(longitude) - radians(@longitude)) + sin(radians(@latitude)) * sin(radians(latitude)))) AS distance FROM landRegistry HAVING distance < 1 ORDER BY distance LIMIT 0, 20')
     //const sql = db.prepare('SELECT * from landRegistry WHERE latitude != 0 AND latitude BETWEEN (@latitude - @radius) AND (@latitude + @radius) AND longitude BETWEEN (@longitude - @radius) AND (@longitude + @radius) ORDER BY abs(@latitude - latitude) + abs(@longitude - longitude) ASC limit 20')
-    const sql = db.prepare('SELECT * from landRegistry ORDER BY (latitude - @latitude) * (latitude - @latitude) + ((longitude - @longitude) * @radius) * ((longitude - @longitude) * @radius) ASC limit 20')
+    const sql = db.prepare('SELECT inspireID, coordinates from landRegistry ORDER BY (latitude - @latitude) * (latitude - @latitude) + ((longitude - @longitude) * @radius) * ((longitude - @longitude) * @radius) ASC limit @limit')
+    const limit = (ctx.params.limit && ctx.params.limit <= 500) ? ctx.params.limit : 100
     const data = sql.all({
         longitude: parseFloat(ctx.params.longitude),
         latitude: parseFloat(ctx.params.latitude),
         // Maybe default to 2 instead of 0.0015
-        radius: parseFloat(ctx.params.radius) | 0.0015
+        radius: 20,
+        limit 
     })
     if (data) {
         data.forEach(item => {
             item.coordinates = JSON.parse(item.coordinates)
-            delete item.latitude
-            delete item.longitude
         })
         ctx.body = {
             data,
@@ -65,7 +63,7 @@ router.get('/nearby/:latitude/:longitude/:radius?', async ctx => {
 })
 
 server.use(router.routes())
-server.listen(80)
+server.listen(8080)
 
 /*
 db.serialize(() => {
