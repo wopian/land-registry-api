25542148fcb59d947c06b39b659674f42cdd9eee
James Harris <wopian@wopian.me>
2020-02-03 15:13:37 +0000

    feat: improve sql performance + change radius argument to meters

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ const Router = require('koa-router')
 const bodyParser = require('koa-body')()
 const db = require('./database')
 
+function radians(degrees) {
+    return degrees * (Math.PI / 180)
+}
+
+db.function('getNearest', { deterministic: true, varargs: true }, (latDB, lat, longDB, long) => {
+    return 6371 * Math.acos(
+        Math.cos(radians(lat)) *
+        Math.cos(radians(latDB)) *
+        Math.cos(radians(longDB) - radians(long)) +
+        Math.sin(radians(lat)) *
+        Math.sin(radians(latDB))
+    )
+})
+
 const server = new Koa()
 const router = new Router()
 
@@ -14,7 +28,6 @@ router.get('/', ctx => {
         }
     }
 })
-
 
 // http://localhost:3001/inspireid/14835677
 router.get('/inspireid/:id', bodyParser, async ctx => {
@@ -36,26 +49,23 @@ router.get('/inspireid/:id', bodyParser, async ctx => {
 
 // http://localhost:3001/nearby/51.6245033595047/-3.93390545244112?limit=500&radius=1000m
 router.get('/nearby/:latitude/:longitude', async ctx => {
-    const sql = db.prepare('SELECT inspireID, coordinates FROM landRegistry WHERE (latitude-@latitude)*(latitude-@latitude) + (longitude-@longitude)*(longitude-@longitude) < @radius*@radius LIMIT @limit')
+    //const sql = db.prepare('EXPLAIN QUERY PLAN SELECT inspireID, coordinates FROM landRegistry WHERE (latitude-@latitude)*(latitude-@latitude) + (longitude-@longitude)*(longitude-@longitude) < @radius LIMIT @limit')
+    const sql = db.prepare('SELECT inspireID, coordinates, getNearest(latitude, @latitude, longitude, @longitude) AS distance FROM landRegistry WHERE distance < @radius ORDER BY distance LIMIT 0,@limit')
 
     const limit = (ctx.query.limit > 0 && ctx.query.limit <= 500) ? ctx.query.limit : 200
-    const radius = {
-        '100m': 0.00053995244,
-        '200m': 0.00107995244,
-        '500m': 0.00269975244,
-        '1000m': 0.00539955244
-    }[ctx.query.radius || '100m']
+    const radius = (ctx.query.radius > 0 && ctx.query.radius <= 1000) ? ctx.query.radius / 1000 : 0.1
 
     const data = sql.all({
         longitude: parseFloat(ctx.params.longitude),
         latitude: parseFloat(ctx.params.latitude),
-        radius,
+        radius: radius * radius,
         limit
     })
     if (data) {
         data.forEach(item => {
             item.coordinates = JSON.parse(item.coordinates)
             item.inspireID
+            item.distance = ~~(item.distance * 1000) // meters
         })
         ctx.body = {
             data,
